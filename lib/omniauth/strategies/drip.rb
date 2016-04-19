38770779b21efa0f82937477efd74d2ed9829989
Tim Breitkreutz <tim@sbrew.com>
2016-04-18 17:05:01 -0700

    Merge Sprout Video and Waitlisted versions

--- lib/omniauth/strategies/drip.rb
+++ lib/omniauth/strategies/drip.rb
@@ -6,11 +6,38 @@ module OmniAuth
 
       option :name, 'drip'
 
+      option :access_token_options, {
+        :header_format => 'Bearer %s',
+        :param_name => 'token'
+      }
+
       option :client_options, {
         :site => 'https://api.getdrip.com/v2',
         :authorize_url => 'https://www.getdrip.com/oauth/authorize',
         :token_url => 'https://www.getdrip.com/oauth/token'
       }
+
+      uid { access_token.client.id }
+
+      info do
+        {
+          email: user_info["users"][0]["email"]
+        }
+      end
+
+      extra do
+        {
+          accounts: raw_info["accounts"]
+        }
+      end
+
+      def user_info
+        @user_info ||= JSON.parse(access_token.get("/user").body)
+      end
+
+      def raw_info
+        @raw_info ||= JSON.parse(access_token.get("/accounts").body)
+      end
     end
   end
 end
